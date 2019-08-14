7b53cea562cbedd6bb5f8597ea8df4475e15c062
Cribbdo <cribbdo@gmail.com>
2019-08-14 17:42:14 +0200

    changing response

--- server.js
+++ server.js
@@ -31,4 +31,4 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
-app.listen(process.env.PORT || 3000, () => { console.log(`app is running on port ${process.env.PORT}`); });
+app.listen(process.env.PORT || 3000, () => { console.log(`app is running on port ${process.env.PORT}`) });
